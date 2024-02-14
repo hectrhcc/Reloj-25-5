0361faa1d568cae5910e04894bb7bbd8641c20ae
Hector <hcontrerascorvacho@gmail.com>
2024-02-13 21:39:29 -0300

    otro avance++

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ let tiempo=0;
 class Reloj extends React.Component{
   constructor(props){
     super(props);
+    this.audioRef = React.createRef();
     this.state = {
     clicked: false,
     break:5,
     session:25,
     seconds:0,
     temporizador:false,
-    name:'Session'
+    name:'Session',
+    activo:true
     }
      this.handleClick = this.handleClick.bind(this);
      this.incrementBreak = this.incrementBreak.bind(this)
@@ -23,60 +25,75 @@ class Reloj extends React.Component{
     this.startstop = this.startstop.bind(this)
   }
    incrementBreak() {
+     if(this.state.activo){
      if (this.state.break < 60){
-    this.setState(state =>({
+    this.setState((state) =>({
        break: state.break+1 
     }));
+   }else if(this.state.break > 60){
+     ;
    }
   }
+  }
    decrementBreak() {
+     if(this.state.activo){
      if (this.state.break>1){
-    this.setState(state =>({
+    this.setState((state) =>({
        break:state.break-1 
     }));
-   }
+   }else if(this.state.session<=0){
+    ;
+   }  
+     }
   }
    reset() {
     this.setState({
        break: 5,
        session:25,
        seconds:0,
-      name:'Session'
+      name:'Session',
     });
       clearInterval(tiempo);//detiene el tiempo
+     this.audioRef.current.pause();
+    this.audioRef.current.currentTime = 0;
   }
   incrementSession() {
+    if(this.state.activo){
     if(this.state.session<60){
-    this.setState(state =>({
+    this.setState((state) =>({
        session:state.session+1
     }));
+  }else if(this.state.break > 60){
+     ;
+  }
   }
   }
    decrementSession() {
+     if(this.state.activo){
      if(this.state.session>1){
-    this.setState(state =>({
+    this.setState((state) =>({
        session:state.session-1
     }));
-   }else if(this.state.session<0){
+   }else if(this.state.session<=0){
     ;
    }  
   }
+   }
   startstop() {
+    this.state.activo=false;
   if(this.state.temporizador){
     this.pauseTimer(); //pausa
   } else {
     this.resumeTimer();//reanuda
   }
-
   this.setState(prev => ({
     temporizador: !prev.temporizador
   }));
 }
-
 pauseTimer() {
+   this.state.activo=true;
   clearInterval(tiempo);
 }
-
 resumeTimer() {
   if(this.state.seconds === 0) {
     this.resetCount();
@@ -86,35 +103,56 @@ if(this.state.session>=0){
     this.startTimer();
 }
 }
-
 startTimer() {
-
   tiempo = setInterval(() => {
-
-    // Lógica de decremento
-
-    if(this.state.session=== 0 && this.state.seconds===0) {
-      clearInterval(tiempo);
+    if(this.state.session===0 && this.state.seconds===0) {
+       this.audioRef.current.play();
+      this.setState((state) =>({
+       name:'Break' ,
+        seconds: state.seconds === 0 ? 59 : state.seconds - 1,
+        session: state.seconds === 0 ? state.break-1 : state.break
+    }));
     } else{
        this.setState(prevState => ({
       seconds: prevState.seconds === 0 ? 59 : prevState.seconds - 1,
       session: prevState.seconds === 0 ? prevState.session - 1 : prevState.session
     }));
     }
-
   }, 1000);
 
 }  
-
 resetCount() {
-  if(this.state.session>=0){
-  this.setState({
-    seconds: 59,
-    session: this.state.seconds === 0 ? this.state.session - 1 : this.state.session
-  });  
-}    
+   if (this.state.name === 'Session') {
+    if (this.state.session >= 0) {
+      this.setState({
+        seconds: 59,
+        session: this.state.session - 1,
+      });
+    }
+    if (this.state.session === 0) {
+      this.setState({
+        name: 'Break',
+        seconds: 0,
+      });
+    }
+  } else if (this.state.name === 'Break') {
+    if (this.state.seconds === 59) {
+      this.setState({
+        break: this.state.break - 1,
+      });
+    }
+    this.setState({
+      seconds: this.state.seconds === 0 ? 59 : this.state.seconds - 1,
+    });
+    if (this.state.seconds === 0) {
+      this.setState({
+        break: this.state.break - 1,
+      });
+    }
+  }
 }
   componentDidMount() {
+    if(this.state.name=='Session'){
   if(this.state.temporizador ){  
   tiempo = setInterval(() => {
     this.setState((prevState) => ({ 
@@ -122,15 +160,21 @@ resetCount() {
     }))  
   }, 1000);
  }
-    
+ }
+  else if(this.state.name=="Break"){
+     tiempo = setInterval(() => {
+ this.setState((prevState) => ({
+  seconds: prevState.seconds === 0 ? 59 : prevState.seconds - 1,
+ session: prevState.seconds === 0 ? prevState.break-1 : prevState.break
+  }))        
+}, 1000);
+ }  
 }
-
 componentWillUnmount() {
   if(this.state.temporizador){
   clearInterval(tiempo);
     }
 }
-
   handleClick(){
     this.setState(prevState => ({
     clicked: true
@@ -148,7 +192,6 @@ componentWillUnmount() {
      <div  id="break-increment" onClick={this.incrementBreak}><i class="fa fa-arrow-up fa-sm"></i></div>
      </div>
      </div>  
-
 <div  id="session-label">Session Length
   <div className="flechas">
   <div  id="session-decrement" onClick={this.decrementSession}><i class="fa fa-arrow-down fa-sm"></i></div>
@@ -162,7 +205,7 @@ componentWillUnmount() {
   {`${String(this.state.session).padStart(2, '0')}:${String(this.state.seconds).padStart(2, '0')}`}
 </div>
     </div>
-   
+     <audio src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav" id="beep" ref={this.audioRef}></audio>
   <span id="start_stop" onClick={this.startstop}>
     <i class="fa fa-play fa-sm"></i>      
       <i class="fa fa-pause fa-sm"></i>
@@ -173,5 +216,4 @@ componentWillUnmount() {
     )
   }
 }
-
 ReactDOM.render(<Reloj />,document.getElementById('root'));
